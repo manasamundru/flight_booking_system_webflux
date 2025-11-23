717452daee0970aa280626aead2dc2e50f21081f
manasamundru <manasa22bce9544@vitapstudent.ac.in>
2025-11-23 19:12:56 +0530

    added pnr generator in the util

---
+++ FlightBookingUsingWebflux/src/main/java/com/flightapp/util/PnrGenerator.java
@@ -0,0 +1,10 @@
+package com.flightapp.util;
+
+import java.util.UUID;
+
+public class PnrGenerator {
+    public static String generate() {
+    	String pnr = UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+    	return pnr;
+    }
+}
