ec3aace4b786dfcefce6eead28680a14b8ff5960
manasamundru <manasa22bce9544@vitapstudent.ac.in>
2025-11-23 19:40:32 +0530

    added the booking controller for booking ticket

---
+++ FlightBookingUsingWebflux/src/main/java/com/flightapp/controllers/BookingController.java
@@ -0,0 +1,33 @@
+package com.flightapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.flightapp.dto.*;
+
+import com.flightapp.services.BookingService;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/flight")
+public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @PostMapping("/booking/{flightId}")
+    public Mono<ResponseEntity<String>> bookTicket(
+            @PathVariable String flightId,
+            @RequestBody BookingRequest request) {
+
+        return bookingService.bookTicket(flightId, request)
+                .map(pnr -> ResponseEntity.status(HttpStatus.CREATED).body(pnr));
+    }
+    
+}
